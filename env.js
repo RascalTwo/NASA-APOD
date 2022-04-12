f97fd10e74e3324a586fb1353f53e157ce7246a4
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-12 18:17:43 -0400

    Make API key dynamic

--- env.js
+++ env.js
@@ -1,1 +1,11 @@
-const API_KEY = 'DEMO_KEY'
+const API_KEY = (() => {
+	const localAPIKey = localStorage.getItem('apod-api');
+	if (localAPIKey) return localAPIKey;
+
+	const key = prompt('Enter NASA API Key for full un-rate-limited acces', 'DEMO_KEY') || 'DEMO_KEY';
+	if (key !== 'DEMO_KEY' && confirm('Save this NASA API Key to localStorage for continued used?')) {
+		localStorage.setItem('apod-api', key);
+	}
+	return key;
+})();
+
